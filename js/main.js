bb0c9b07dc5274a9aca3ca511ef02f67bd77c4f3
Max <yudinmax4444@gmail.com>
2020-11-27 17:20:34 +0200

    trainee

---
+++ js/main.js
@@ -0,0 +1,16 @@
+let admin;
+let name;
+
+admin = `John`;
+name = admin;
+alert(admin)
+
+let ourPlanetName = `земля`;
+
+let yourName = prompt(`Укажите ваше имя`, `Yoda`)
+
+alert(`hi ${yourName}`)
+let a = prompt("Первое число?", 1);
+let b = prompt("Второе число?", 2);
+
+alert(+a + +b);
